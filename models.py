3f0d1089c8d47a1529037d41f698669e50839972
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-18 07:16:58 +0300

    Create Bird model

---
+++ models.py
@@ -0,0 +1,15 @@
+# models.py
+
+from config import db
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Bird(db.Model, SerializerMixin):
+    __tablename__ = 'birds'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Bird {self.name} | Species: {self.species}>'
