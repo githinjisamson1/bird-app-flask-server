951e984a2c951680c7727ef7a2b25a068253200f
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-18 07:16:28 +0300

    Set up basic flask configuration

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
